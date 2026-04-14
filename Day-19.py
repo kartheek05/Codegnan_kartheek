620b879fb321cf4c3dffd35d4e34674302086dbe
kartheek05 <kumarkartheek2005@gmail.com>
2026-04-14 12:45:45 +0530

    Modules

---
+++ Day-19.py
@@ -0,0 +1,49 @@
+'''
+Modules---> A module is a pyhton is simply file that contains pyhton code(functions,variables,classes)
+---> to use this modules, we have to use a keyword called (import) before the module.
+
+---------------------------------------------------------------Types of modules----------------------------------------------
+User-defines modules-----> This is developed by the user or programmer inside a file of pyhton code and used by called import
+                                                with filename
+Syntax -----> import(keyword) file_name
+                     file_name.functionality
+import add               #user-definedmodules
+print(add.sub(4,5))
+print(add.add(4,5))
+print(add.mul(4,5))
+print(add.div(4,5))
+print(add.kartheek+3)
+'''
+
+
+'''
+1.Build-IN or inbuilt-------------> already these modules will come with installation and they are ready to use in the program.
+                                                  This are developed by the developers.
+Syntax -----> import(random) module_name
+                     module_name.functionality
+
+import math
+print(math.sqrt(16))                                                  
+'''
+import random
+print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
